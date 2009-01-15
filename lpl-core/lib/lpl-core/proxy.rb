5f51948443db585a366c039b340d5fe930bfa96f
Fabien Franzen <info@atelierfabien.be>
2009-01-15 21:18:14 +0100

    Moved public/images to public/stylesheets/assets

---
+++ lpl-core/lib/lpl-core/proxy.rb
@@ -0,0 +1,78 @@
+module LplCore
+  class Proxy
+    
+    # This class is available from within LplCore controllers, as well as from
+    # LplCore Extension controllers through the 'core' attribute.
+    #
+    # Most methods defined here are analogous to extension/slice instance methods
+    # but in this case they apply specifically to LplCore only.
+    
+    attr_reader :controller, :config, :info
+    
+    def initialize(controller)
+      @controller = controller
+      @config = LplCore.config
+      @info = @config[:info] || {}
+    end
+    
+    def url(*args)
+      opts = args.last.is_a?(Hash) ? args.pop : {}
+      route_name = args[0].is_a?(Symbol) ? args.shift : :index      
+      routes = Merb::Slices.named_routes[:lpl_core]
+      unless routes && route = routes[route_name]
+        raise Merb::Router::GenerationError, "Named route not found: #{route_name}"
+      end      
+      args.push(opts)
+      route.generate(args)
+    end
+    
+    def require_js(*js)
+      js.flatten!
+      options = js.last.is_a?(Hash) ? js.pop : {}
+      assets = js.map { |asset| javascript_path("#{asset}.js") }
+      controller.require_js(*(assets << options))
+    end
+    
+    def require_css(*css)
+      css.flatten!
+      options = css.last.is_a?(Hash) ? css.pop : {}
+      assets = css.map { |asset| stylesheet_path("#{asset}.css") }
+      controller.require_css(*(assets << options))
+    end
+    
+    def image_path(*segments)
+      public_path_for(:image, *segments)
+    end
+    
+    def javascript_path(*segments)
+      public_path_for(:javascript, *segments)
+    end
+    
+    def stylesheet_path(*segments)
+      public_path_for(:stylesheet, *segments)
+    end
+    
+    def public_path_for(type, *segments)
+      LplCore.public_path_for(type, *segments)
+    end
+    
+    def app_path_for(type, *segments)
+      LplCore.app_path_for(type, *segments)
+    end
+    
+    def slice_path_for(type, *segments)
+      LplCore.slice_path_for(type, *segments)
+    end
+    
+    # delegation back to controller
+    
+    def method_missing(method, *args, &block)
+      if @controller.respond_to?(method)
+        @controller.send(method, *args, &block)
+      else
+        super
+      end
+    end
+    
+  end
+end
