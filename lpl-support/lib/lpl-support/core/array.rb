973c945f9b199f9a010017449b5b5b50c355a48b
Fabien Franzen <info@atelierfabien.be>
2009-02-15 17:31:57 +0100

    Header icons are now showing .active state

--- lpl-support/lib/lpl-support/core/array.rb
+++ lpl-support/lib/lpl-support/core/array.rb
@@ -3,6 +3,23 @@ class Array
   module PaginationMethods
     attr_accessor :total, :page, :pages, :per_page, :offset
     attr_accessor :prev_url, :next_url
+    
+    def prev_page
+      self.page - 1
+    end
+    
+    def prev_page?
+      self.page > 1
+    end
+    
+    def next_page
+      self.page + 1
+    end
+    
+    def next_page?
+      self.page < self.pages
+    end
+    
   end
   
   def paginate(page = 1, per_page = 5)
@@ -17,7 +34,11 @@ class Array
     elsif self.offset < 0
       self.offset = 0
     end
-    self.slice!(self.offset, self.per_page)
+    self.slice(self.offset, self.per_page)
+  end
+  
+  def paginate!(page = 1, per_page = 5)
+    self.replace(paginate(page, per_page))
   end
   
 end
